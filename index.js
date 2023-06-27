4bdfc540a470f36d705efd0842439360dff28b0f
Pavel Potemkin <specialforbtsu@gmail.com>
2023-06-27 19:26:44 +0700

    added socket service

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+const http = require('http').Server(app);
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
